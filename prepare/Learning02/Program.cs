cfb72a1f1979bcc6160b7045638d6ca9b57683d8
adampetersen100 <pet23079@byui.edu>
2025-01-23 22:53:13 -0700

    Started work on the journal program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+
+        Job company1 = new Job();
+        company1._company = "Microsoft";
+
+        Job company2 = new Job();
+        company2._company = "Apple";
+
+        Job start1 = new Job();
+        start1._startYear = 1999;
+
+        Job end1 = new Job();
+        end1._endYear = 2023;
+
+        Console.WriteLine(job1._jobTitle);
+        Console.WriteLine(company1._company);
+        Console.WriteLine(company2._company);
     }
 }
