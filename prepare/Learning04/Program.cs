277d5e2c1260a7f3acf2f51d9e560d1c457fc1b5
adampetersen100 <pet23079@byui.edu>
2025-02-20 22:23:35 -0700

    Finished W07 Learning Activity assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Adam", "Calculus");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("James", "Algebra", "Chapter 2.1", "10-21");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Johnny", "American History", "The American Revolution");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
